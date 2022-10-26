dcb39b3721f4d5c4166eb8256a80c64a1c46d855
ejc224 <83097225+ejc224@users.noreply.github.com>
2022-10-25 23:25:41 -0400

    Update Simulate.java
    - removed repetitive code and condensed into methods
    - updated and added descriptive comments
    - next: improving the read out of data to identify any errors

--- Simulate.java
+++ Simulate.java
@@ -1,4 +1,7 @@
-// 10.25.22   
+// updated: 10/25
+/// removed repetitive code and condensed into methods
+/// updated and added descriptive comments
+/// next: improving the read out of data to identify any errors 
 
 import java.util.Random;
 import java.util.Arrays;
@@ -10,131 +13,59 @@ import java.io.IOException;
 
 public class Simulate {
     public static void main(String[] args){
-        // creating a square with sides = 2
+        /***
+         * creating a square with sides = 2
+         ***/
         int side = 2;
         Square square = new Square(side);
         int sqArea = square.getArea(side);
         System.out.println("The area of the square is: "+sqArea);
         System.out.println("The distance from the center of the square to the edges is: "+side/2);
         /***
-         * calculating the furthest fistance from the side of the square to the center of the circle
+         * calculating the furthest distance from the side of the square to the center of the circle
          * visually: I am creating a triangle whose longest side is from the corner of the square to the center of the square. The other two sides of the triangle are equal to side/2
-         */
+         ***/
         int sidesT = (side/2);
         double sqsidesT = Math.pow(sidesT, 2);
         double hypotenuse = Math.sqrt(sqsidesT+sqsidesT);
         System.out.println("The length of the hypotenuse is: "+hypotenuse);
-
-        // creating a circle with radius side/2 (to fit inside the square)
-        Circle circle = new Circle(side/2);
-        int radius = circle.getRadius();
-        System.out.println("The radius of the circle is: "+ radius);
-
         /***
-         * simulate a number from 0-1  
-         * ** if the number is less than 1 it is inscribed in the circle and the square if the number is
-         * ** if the number is greater than 1 it is not inscribed in the circle
-         * ** if the number is equal to 1, it counts as in the circle
-         * */
-        // when n = 100
-        int inCircle100 = 0;
-        for(int i=0; i<100; i++){
-            double randomNumber = generateRandomNumber(hypotenuse);
-            if (randomNumber <= 1){
-                inCircle100++;
-            }
-        }
-        /***
-         * Calculating the proportion of inCircle values to outCircle values
-         * area of circle / area of square = # of values <= 1 / # of total values generated
-         * looking for the area of circle
-         */
-        double denominator100 = inCircle100/100.00;
-        double cArea100 = (double) denominator100*sqArea;
-
-        // when n = 1000
-        int inCircle1000 = 0;
-        for(int i=0; i<1000; i++){
-            double randomNumber = generateRandomNumber(hypotenuse);
-            if (randomNumber <= 1){
-                inCircle1000++;
-            }
-        }
-        double denominator1000 = inCircle1000/1000.00;
-        double cArea1000 = (double) denominator1000*sqArea;
-        
-        // when n = 10000
-        int inCircle10000 = 0;
-        for(int i=0; i<10000; i++){
-            double randomNumber = generateRandomNumber(hypotenuse);
-            if (randomNumber <= 1){
-                inCircle10000++;
-            }
-        }
-        double denominator10000 = inCircle10000/10000.00;
-        double cArea10000 = (double) denominator10000*sqArea;
-
-        // when n = 100000
-        int inCircle100000 = 0;
-        for(int i=0; i<100000; i++){
-            double randomNumber = generateRandomNumber(hypotenuse);
-            if (randomNumber <= 1){
-                inCircle100000++;
-            }
-        }
-        double denominator100000 = inCircle100000/100000.00;
-        double cArea100000 = (double) denominator100000*sqArea;
-        
-        // when n = 1000000
-        int inCircle1000000 = 0;
-        for(int i=0; i<1000000; i++){
-            double randomNumber = generateRandomNumber(hypotenuse);
-            if (randomNumber <= 1){
-                inCircle1000000++;
-            }
-        }
-        double denominator1000000 = inCircle1000000/1000000.00;
-        double cArea1000000 = (double) denominator1000000*sqArea;
-
-        // when n = 10000000
-        int inCircle10000000 = 0;
-        for(int i=0; i<10000000; i++){
-            double randomNumber = generateRandomNumber(hypotenuse);
-            if (randomNumber <= 1){
-                inCircle10000000++;
-            }
-        }
-        double denominator10000000 = inCircle10000000/10000000.00;
-        double cArea10000000 = (double) denominator10000000*sqArea;
-
-        /***
-         * Array to store the cArea values in for each values of n
-         */
-        double [] areaVals = new double[6];
-        areaVals[0] = cArea100;
-        areaVals[1] = cArea1000;
-        areaVals[2] = cArea10000;
-        areaVals[3] = cArea100000;
-        areaVals[4] = cArea1000000;
-        areaVals[5] = cArea10000000;
-        System.out.println(Arrays.toString(areaVals));
+         * Creating two arrays: nVals and areaVals
+         * nVals to hold the number of samples which increases by 10x with each increasing value
+         * areaVals to hold the calculation of the area calculated
+         ***/
         int [] nVals = {100, 1000, 10000, 100000, 1000000, 10000000};
-        System.out.println(Arrays.toString(nVals));
+        double [] areaVals = new double[nVals.length];
+        for(int i=0; i<areaVals.length; i++){
+            areaVals[i] = calculateArea(nVals[i], hypotenuse, sqArea);
+        }
+        System.out.println(Arrays.toString(areaVals));
 
         /***
-         * start by creating a csv file using nVals and areaVals as the axi
-         */
-        //File f = createFile();
+         * Ensuring the file is created and creating it if it is not
+         * Writing the results to a csv file
+         ****/
+        createFile("darts.txt");
         writeToFile(areaVals, nVals,"darts.txt");
     }
+    /***
+     * Method to generate the random number from 0 to the hypotenuse (max) value
+     * @param max
+     * @return double
+     */
     public static double generateRandomNumber(double max){
         Random rand = new Random();
         double randomNumber = rand.nextDouble(max); // generates a random number from 0 to 2 (unsure what the upper bound is supposed to be)
         return randomNumber;
     }
-    public static File createFile() {
+    /***
+     * Method to create the file
+     * @param filename
+     * @return File
+     ***/
+    public static File createFile(String filename) {
         // creating file
-        File file = new File("darts.txt");
+        File file = new File(filename);
         try{
             if(file.createNewFile()){
                 System.out.println("File created: "+ file.getName());
@@ -148,6 +79,13 @@ public class Simulate {
         }
         return file;
     }
+    /*** 
+     * Method to write to the file
+     * @param array called areaVals type double
+     * @param array called n type int
+     * @param String with the filename
+     * @return void
+    ***/
     public static void writeToFile(double [] areaVals, int [] n, String file){
         try{
             FileWriter writer = new FileWriter(file);
@@ -158,9 +96,34 @@ public class Simulate {
             }
             printer.close();
         }
-        catch (IOException e){
-
+        catch (IOException e){}
+    }
+    /***
+     * Method to calculate the simulate calculating the area
+     * Calculation: it is the proportion of data points inside the circle vs the data points inside the circle + outside the circle
+     * @param n
+     * @param hypotenuse
+     * @param sqArea
+     * @return double which is the estimation of the area of a circle
+     ***/
+    public static double calculateArea(int n, double hypotenuse, int sqArea){
+        int inCircle100 = 0;
+        for(int i=0; i<n; i++){
+            double randomNumber = generateRandomNumber(hypotenuse);
+            if (randomNumber <= 1){
+                inCircle100++;
+            }
         }
+        /***
+         * Calculating the proportion of inCircle values to outCircle values
+         * area of circle / area of square = # of values <= 1 / # of total values generated
+         * looking for the area of circle
+         ***/
+        double ndouble = (double) n;
+        double denominator100 = inCircle100/ndouble;
+        double cArea100 = (double) denominator100*sqArea;
+        return cArea100;
     }
 }
+
 
